fed90c78f7d5f556d827e72a39b0c515afeac8da
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-02-17 17:00:12 +0400

    added sound

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import logo from './logo2.svg';
+import logo from './logo.svg';
+import soundfile from './sound.mp3'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Arriba</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>Arriba</p>
+                <audio controls>
+                    <source src={soundfile} type="audio/mpeg"/>
+
+                </audio>
+            </header>
+        </div>
+    );
 }
 
 export default App;
