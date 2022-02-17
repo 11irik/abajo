7b53d156523839d1e10edc6ec997a3df6ea1dd86
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-02-17 15:48:53 +0400

    arriba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ABAJO</p>
+        <p>Arriba</p>
       </header>
     </div>
   );
