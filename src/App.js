9749b8fa34f3b4a49c44b297f005f2c044609d5a
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-02-17 17:01:01 +0400

    abajo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>Arriba</p>
+                <p>Abajo</p>
                 <audio controls>
                     <source src={soundfile} type="audio/mpeg"/>
 
