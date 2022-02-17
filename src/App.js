051dc1d1400d1c8772c16ea4c33a406edba121cc
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-02-17 14:44:06 +0400

    added oleg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo2.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>ABAJO</p>
       </header>
     </div>
   );
